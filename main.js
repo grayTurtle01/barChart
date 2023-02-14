2ecf96ac58a01e45414e4ab8e6b022547f6741c8
grayTurtle01 <the.correo@protonmail.com>
2023-02-14 07:44:30 -0600

    user-story #3: y-axis

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-w = 600
-h = 400
-padding = 20
+w = 700
+h = 500
+padding = 50
 
 svg = d3.select('body')
 			  .append('svg')
@@ -12,10 +12,21 @@ xScale = d3.scaleLinear()
            .domain([1900, 2015])
            .range([padding, w-padding])
 
+yScale = d3.scaleLinear()
+					 .domain([0, 18000])
+					 .range([h-padding, padding])
+
 
 xAxis = d3.axisBottom(xScale)
+
+yAxis = d3.axisLeft(yScale)
 
 svg.append('g')
    .attr('transform', `translate(0, ${h-padding})`)
    .attr('id', 'x-axis')
    .call(xAxis)
+
+svg.append('g')
+	 .attr('transform', `translate(${padding}, 0)`)
+	 .attr('id', 'y-axis')
+	 .call(yAxis)	
